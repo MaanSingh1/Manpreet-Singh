efec14482f47eda086aba6ac7517638c7c57641f
MaanSingh1 <93256107+MaanSingh1@users.noreply.github.com>
2024-10-14 13:16:06 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const introSection = document.getElementById('intro');
+    introSection.innerHTML += '<p>Sono contento che tu sia qui! Esplora e scopri di più su di me.</p>';
+});
